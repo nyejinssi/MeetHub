7bd1cb8f15d1bc40359dd9fa34cb791da160dcd2
Han <hyejin3998@gmail.com>
2024-01-03 21:15:55 +0900

    s1.2.message

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,1 +1,19 @@
+// 웹소켓 연결 생성
 var socket = new WebSocket(`ws://${window.location.host}`);
+
+// 소켓이 연결되면 발생하는 이벤트 핸들러
+socket.addEventListener("open", () => {//socket이 connection을 open했을 때
+    console.log("Connected to Server ✅ ");
+})
+// 서버로부터 메시지를 수신하면 발생하는 이벤트 핸들러
+socket.addEventListener("message", (message) => {
+    console.log("New Message: ", message.data );
+});
+// 소켓이 닫히면 발생하는 이벤트 핸들러
+socket.addEventListener("close", () => {
+    console.log("Disconnected from Server ❌");
+});
+// 10초 후에 "hello from the browser" 메시지를 서버로 전송
+setTimeout(() => {
+    socket.send("hello from the browser");
+}, 10000); 
--- src/server.js
+++ src/server.js
@@ -3,31 +3,49 @@ import express from "express";
 import http from "http";
 import { WebSocketServer } from 'ws';
 
-const app = express();
+const app = express(); // Express 애플리케이션 생성
+const __dirname = path.resolve(); // 현재 디렉토리 경로 해결
 
-const __dirname = path.resolve();
-
+// Pug 템플릿을 위한 뷰 엔진 및 뷰 디렉토리 설정
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "src", "public", "views"));
-app.use(express.static(path.join(__dirname, "src", "public")));  // 수정된 부분
 
+// 'public' 디렉토리에서 정적 파일 제공
+app.use(express.static(path.join(__dirname, "src", "public")));
+
+// Pug 템플릿인 "home.pug"으로 루트 경로 ("/") 처리
 app.get("/", (req, res) => res.render("home.pug"));
-// 루트 경로("/")를 제외한 모든 경로에 대한 GET 요청 핸들러
+
+// 루트 경로 ("/")를 제외한 모든 경로에 대한 GET 요청 처리
 app.get("/*", (req, res) => {
-    // 클라이언트에게 루트 경로("/")로 리디렉션(재지정)하는 응답을 보냄
+    // 클라이언트에게 루트 경로 ("/")로 리디렉션하는 응답 전송
     res.redirect("/");
 });
 
-
+// 서버 리스닝을 처리할 함수 정의
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
+// Express 애플리케이션을 사용하여 HTTP 서버 생성
 const server = http.createServer(app);
-const wss = new WebSocketServer({server}); 
 
-function handleConnection(socket){ // 주의사항 : 괄호가 비면 안됨
-    console.log(socket)
-}
+// HTTP 서버에 연결된 WebSocket 서버 생성
+const wss = new WebSocketServer({server});
 
-wss.on("connection", handleConnection);
+// WebSocket 연결에 대한 이벤트 핸들러
+wss.on("connection", (socket)=>{
+    // 연결된 클라이언트에게 메시지 전송
+    socket.send("hello"); //데이터 보내기
 
-server.listen(3000, handleListen);
+    // 소켓 닫힘에 대한 이벤트 핸들러
+    socket.on("close", ()=> console.log("Disconnected from Server ❌"));
+
+    // 클라이언트로부터 수신된 메시지에 대한 이벤트 핸들러
+    socket.on("message", (message)=>{
+        console.log(message);
+    });
+
+    // 연결이 성립되었다는 로그 표시
+    console.log("Connected to Browser ✅ ");
+});
+
+server.listen(3000, handleListen); //서버 시작
