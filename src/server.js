96937a1cbe5f8e0f0a59db460c43c380b37dd991
Han <hyejin3998@gmail.com>
2024-01-03 20:06:47 +0900

    s0.3.2

--- src/server.js
+++ src/server.js
@@ -10,6 +10,12 @@ app.set("views", path.join(__dirname, "src", "public", "views"));
 app.use(express.static(path.join(__dirname, "src", "public")));  // 수정된 부분
 
 app.get("/", (req, res) => res.render("home.pug"));
+// 루트 경로("/")를 제외한 모든 경로에 대한 GET 요청 핸들러
+app.get("/*", (req, res) => {
+    // 클라이언트에게 루트 경로("/")로 리디렉션(재지정)하는 응답을 보냄
+    res.redirect("/");
+});
+
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 app.listen(3000, handleListen);
